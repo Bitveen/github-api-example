16b6345e707ff68f71b26881dd4b7b7cce9dd841
Max <bitveen@gmail.com>
2017-01-17 22:14:20 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        bundle: __dirname + '/src/index.jsx',
+        style: __dirname + '/src/style/main.scss'
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname + '/public'
+    },
+    resolve: {
+        alias: {},
+        extensions: ['', '.js', '.jsx']
+    },
+    devtool: 'cheap-module-source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                include: __dirname + '/src',
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png$|\.jpe?g$|\.gif$/,
+                loader: 'file-loader'
+            }
+
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css', {
+            allChunks: true
+        })
+    ]
+
+
+
+
+};
